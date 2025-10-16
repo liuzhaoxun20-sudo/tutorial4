6e20a16f33fcb65e600bdd48c62701322418b345
liuzhaoxun20-sudo <liuzhaoxun20@gmail.com>
2025-10-16 15:19:23 +0800

    Add failing subtraction test

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -10,5 +10,10 @@ public class CalcTest {
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
-
+    @Test
+    public void testSubtraction() {
+        Calc c = new Calc();
+        // 这个测试会失败，因为 subtract 方法还没有实现
+        assertEquals(2, c.subtract(4, 2));
+    }
 }
